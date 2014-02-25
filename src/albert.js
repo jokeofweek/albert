a3d9869553d555ce19ad63a6cbd44ee501dcde09
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-02-24 23:17:01 -0500

    Added support for swapping out themes.

--- src/albert.js
+++ src/albert.js
@@ -1,21 +1,29 @@
+var Albert = {};
+
+// Wrap Albert in its own scope so that we don't lose anything.
+(function(){
 var fs = require('fs');
 var path = require('path');
-
-var Albert = {};
 
 // Setup the Albert root object.
 Albert.DATA_PATH = path.join(process.env.APPDATA, 'Albert');
 Albert.PLUGINS_PATH = path.join(Albert.DATA_PATH, 'Plugins');
+Albert.THEMES_PATH = path.join(Albert.DATA_PATH, 'Themes');
 
-// Run the first time Albert is initialized.
+/**
+ * Performs any initial Albert setup.
+ * @param  {document} document The DOM.
+ */
 Albert.start = function() {
-// Ensure the Albert root path exists.
+  // Ensure the Albert root path exists.
   if (!fs.existsSync(Albert.DATA_PATH)) fs.mkdirSync(Albert.DATA_PATH);
   // Load plugins
   Albert.reloadPlugins();
 };
 
-// Define the functions for reloading plugins.
+/**
+ * Reloads all Albert plugins from the PLUGINS_PATH.
+ */
 Albert.reloadPlugins = function() {
   // Ensure the path exists.
   if (!fs.existsSync(Albert.PLUGINS_PATH)) fs.mkdirSync(Albert.PLUGINS_PATH);
@@ -33,4 +41,38 @@ Albert.reloadPlugins = function() {
   }
 };
 
-module.exports = Albert;
+/**
+ * This changes the Albert theme to a new theme if found.
+ * @param  {string?} newTheme The name of the new theme, or null to have no theme.
+ * @return {boolean}          A boolean stating whether the theme changed.
+ */
+Albert.changeTheme = function(newTheme) {
+  // Remove the old theme element if present.
+  var themeElement = document.querySelector('#theme-css');
+  if (themeElement) {
+    themeElement.remove();
+  }
+  
+  // If we passed no theme, then return early.
+  if (!newTheme) {
+    return true;
+  }
+
+  // Ensure the theme exists.
+  var themePath = path.join(Albert.THEMES_PATH, newTheme, 'theme.css');
+  if (fs.existsSync(themePath)) {
+    // Create the new theme element
+    var newThemeElement = document.createElement('style');
+    newThemeElement.appendChild(document.createTextNode(fs.readFileSync(themePath)));
+    newThemeElement.id = 'theme-css';
+    document.head.appendChild(newThemeElement);
+
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Start Albert!
+Albert.start();
+})();
--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-var Albert;
-
 (function() {
 
 var listener = new window.keypress.Listener();
@@ -7,8 +5,5 @@ var listener = new window.keypress.Listener();
 listener.simple_combo('ctrl d', function() {
   require('nw.gui').Window.get().showDevTools();
 });
-
-Albert = require('./albert.js');
-Albert.start();
 
 })();
